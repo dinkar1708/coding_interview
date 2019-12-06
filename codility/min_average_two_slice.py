6c94829b464f14ec4f741e760c6f1abc985bb0ad
Dinakar Maurya <dinkar1708@gmail.com>
2019-12-06 17:06:27 +0900

    Update min_average_two_slice.py

--- codility/min_average_two_slice.py
+++ codility/min_average_two_slice.py
@@ -5,11 +5,14 @@ def solution(A):
     """
     https://app.codility.com/demo/results/trainingB955CC-PHS/
     100%
+    Idea-
+    Idea is to calculate the prefix some and find out the average as per given formula in this problem and determine minimum slice starting index
+    Prefix sum - is used to avoid the two loops to calculate the sum of array Items
+    Find min average from P to Q in array and store the minimum index and say it as average of slice from p to q
+    Objective is to find minimum average of other slice, the next slice can be [next q+1 and either include  p,q,q+1 p or exclude q,q+1]
+    We can exclude the slice calculated so far if the current item is less than the average
 
-    idea is to calculate the prefix some and find out the average as per given formula in this problem.
-    take care when average will be decreasing as described in solution
-    find min average from P to Q in array and store the minimum index
-
+    Problem-
     A non-empty zero-indexed array A consisting of N integers is given. A pair of integers (P, Q), such that 0 ≤ P < Q < N,
      is called a slice of array A (notice that the slice contains at least two elements).
       The average of a slice (P, Q) is the sum of A[P] + A[P + 1] + ... + A[Q] divided by the length of the slice.
@@ -32,7 +35,7 @@ def solution(A):
        """
 
     min_index = 0
-
+    print(A)
     # why prefix some? - just to calculate the some between two places without using two loops
     prefix_sum = [0] * len(A)
     prefix_sum[0] = A[0]
@@ -41,37 +44,37 @@ def solution(A):
     print(prefix_sum)
 
     min_avg = sys.float_info.max
-    # start from slice p ie. from start, this is start and act as P in problem statement and Q would be the i as shown below
-    slice_p = 0
-    # print(min_avg)
-    for i in range(1, len(A)):
-        # for i = 1 - at this time calculating average from 0 to 1
-        # length of slice -
-        # as per problem statement (A[P] + A[P + 1] + ... + A[Q]) / (Q − P + 1) - here P is the slice_p and Q is the prefix sum
-        print("From P:slice_p= " + str(slice_p) + "  To Q: i= " + str(i))
-        print("slice length ")
-        print(i - slice_p + 1)
-        # get the some for position P,Q - ie. from slice_p to i positions and davide by the slice length
-        average = (prefix_sum[i] - prefix_sum[slice_p] + A[slice_p]) / (i - slice_p + 1)
-        print("average " + str(average))
+    # start from slice p ie. from start, this is start and act as P in problem statement and Q would be the q_end_index as shown below
+    slice_p_from_index = 0
+    for q_end_index in range(1, len(A)):
+        # for q_end_index = 1 - at this time calculating average from 0 to 1
+        # as per problem statement (A[P] + A[P + 1] + ... + A[Q]) / (Q − P + 1)
+        print("From P:slice_p_from_index= " + str(slice_p_from_index) + "  To Q: i= " + str(q_end_index) + " slice " + str(A[slice_p_from_index:q_end_index + 1]))
+        print("Slice length " + str(q_end_index - slice_p_from_index + 1))
+        # get the some for position P,Q - ie. from slice_p_from_index to i positions and davide by the slice length
+        average = (prefix_sum[q_end_index] - prefix_sum[slice_p_from_index] + A[slice_p_from_index]) / (q_end_index - slice_p_from_index + 1)
         # check for minimum average so far
         if average < min_avg:
             min_avg = average
             # hold the index for minimum average value
-            min_index = slice_p
+            min_index = slice_p_from_index
 
-        # A[i] - this is always the next index for calculation so we have to check its value for next average calculation
-        # this is to find out the next slice_p(P) index
-        # when array item is less than minimum average - reason to check is the minimum average is only affected when array item is less then average
-        if A[i] < min_avg:
-            print("this item index is less than min average ")
-            print(i)
-            slice_p = i
+        print("min_avg " + str(min_avg))
+        # this is to find out the next starting position for slice_p_from_index
+        # idea is to skip all the items found so far up to q_end_index-1, to skip and start from q_end_index we have to proof that the next slice min average
+        # can be less than the current minimum average
+        # that can only happen if and only if current Item A[q_end_index] is less than the minimum average
+        # if so we start taking array slice from current Item A[q_end_index] and make slice with next Item
+        print("Index = " + str(q_end_index) + " Item " + str(A[q_end_index]))
+        if A[q_end_index] < min_avg:
+            print("min index found....Item < min_avg, slice_p_from_index changed to Index")
+            slice_p_from_index = q_end_index
+        print("")
     return min_index
 
 
 result = solution([4, 2, 2, 5, 1, 5, 8], )
-print("Sol " + str(result))
+print("Solution " + str(result))
 
 """
     Run - 
