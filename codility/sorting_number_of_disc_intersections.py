4562667bd51862b70b05d78bf64b97015c5c2534
Dinakar Maurya <dinkar1708@gmail.com>
2019-12-10 12:16:54 +0900

    Create sorting_number_of_disc_intersections.py

---
+++ codility/sorting_number_of_disc_intersections.py
@@ -0,0 +1,121 @@
+def solution(A):
+    """
+    Codility -
+    https://app.codility.com/demo/results/trainingKC23TS-77G/
+    100%
+    
+    Idea is to maintain the start and end point marker for each disc/circle
+    Sort by start position of disc/circle
+    On each start position count the active circle/disc
+    On each end position reduce the active circle/disc
+    Count the total intersection using the active circle in each iteration
+
+    Reference -
+    https://www.youtube.com/watch?v=HV8tzIiidSw
+    http://www.lucainvernizzi.net/blog/2014/11/21/codility-beta-challenge-number-of-disc-intersections/
+    :param A:
+    :return: total intersections
+    """
+    # contains start and end points of circle
+    circle_points = []
+    for i, a in enumerate(A):
+        # store start points and end points of circle
+        # as per problem statement -> The J-th disc is drawn with its center at (J, 0) and radius A[J].
+        # example 0-1, 0+1 second 1-5, 1+5
+        circle_points += [(i - a, True), (i + a, False)]
+    print("Original disc positions " + str(circle_points))
+    # Sort the array of disc, making sure that the start of a disk in a particular point P comes before the end of any disk at P.
+    circle_points.sort(key=lambda x: (x[0], not x[1]))
+    print("Sorted disc positions " + str(circle_points))
+    intersections, active_circles = 0, 0
+
+    # We now walk this array, keeping track of how thick the set of disk is at each new disc (below, the variable is called active_circles).
+    # Furthermore, we increase the number of intersection by active_circles if a new disk starts.
+    for _, is_beginning in circle_points:
+        # active circle found ie. starting disc found
+        if is_beginning:
+            # counting intersections by active circles ie. each active circle must be intersecting the comming new circle,
+            # we already know they are in sorted order by start position
+            intersections += active_circles
+            active_circles += 1
+            print("This is start disc -> intersections " + str(intersections) + " active_circles " + str(active_circles))
+        # ending circle position found, now active circle should be reduced by one
+        else:
+            print("Closing disc found.....")
+            print("Reduce active circle " + str(active_circles))
+            print()
+            active_circles -= 1
+        # ** is 10 to the power of 7
+        if intersections > 10 ** 7:
+            return -1
+
+    return intersections
+
+
+def solution4(A):
+    """
+    this is without using lamda function
+    """
+    print("Right edges")
+    end_pos = [k + v for k, v in enumerate(A)]
+    print(end_pos)
+    end_pos.sort()
+    print(end_pos)
+    print("Left edges")
+    start_pos = [k - v for k, v in enumerate(A)]
+    print(start_pos)
+    start_pos.sort()
+    print(start_pos)
+    print("Sorted right and left edges")
+    print(end_pos)
+    print(start_pos)
+    left_position = 0
+    counter = 0
+    for right_position, v in enumerate(end_pos):
+        print()
+        print("For right_position       " + str(right_position) + str(A[right_position:]))
+        while left_position < len(end_pos) and v >= start_pos[left_position]:
+            print("this left position       " + str(left_position) + str(A[left_position:]))
+            counter += left_position - right_position
+            print("Counter till now         " + str(counter))
+            left_position += 1
+        if counter > 10 ** 7:
+            return -1
+
+    return counter
+
+
+result = solution([1, 5, 2, 1, 4, 0])
+print("")
+print("Solution " + str(result))
+
+"""
+    Original disc positions [(-1, True), (1, False), (-4, True), (6, False), (0, True), (4, False), (2, True), (4, False), (0, True), (8, False), (5, True), (5, False)]
+    Sorted disc positions [(-4, True), (-1, True), (0, True), (0, True), (1, False), (2, True), (4, False), (4, False), (5, True), (5, False), (6, False), (8, False)]
+    This is start disc -> intersections 0 active_circles 1
+    This is start disc -> intersections 1 active_circles 2
+    This is start disc -> intersections 3 active_circles 3
+    This is start disc -> intersections 6 active_circles 4
+    Closing disc found.....
+    Reduce active circle 4
+    
+    This is start disc -> intersections 9 active_circles 4
+    Closing disc found.....
+    Reduce active circle 4
+    
+    Closing disc found.....
+    Reduce active circle 3
+    
+    This is start disc -> intersections 11 active_circles 3
+    Closing disc found.....
+    Reduce active circle 3
+    
+    Closing disc found.....
+    Reduce active circle 2
+    
+    Closing disc found.....
+    Reduce active circle 1
+    
+    
+    Solution 11
+"""
