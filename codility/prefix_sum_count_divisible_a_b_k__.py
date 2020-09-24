73c150f55150e2a1feff2f8e03306c2c6cc830ad
Dinakar Maurya <dinkar1708@gmail.com>
2020-09-24 22:32:58 +0900

    Update prefix_sum_count_divisible_a_b_k__.py

--- codility/prefix_sum_count_divisible_a_b_k__.py
+++ codility/prefix_sum_count_divisible_a_b_k__.py
@@ -1,18 +1,25 @@
+# https://app.codility.com/demo/results/training9D6VJ8-3BG/
 import math
 
 
 def solution(A, B, K):
     """
-    Dinakar
-    https://app.codility.com/demo/results/trainingA9ZXXC-3U9/
-    Find start range and end range
-    start = 6/2 = 3.0 ceil = 3
-    end = 11/2 = 5.5 floor = 6
+    DINAKAR
+
+    A<=i<=B
+    Take floor of B/K
+    11/2 = 5.5 floor->6
+    Take ceil of A/K
+    6/2 = 3 ceil->3
+    return 6-3 + 1
     """
-    # ceil get next start point
-    # floor get last end point
-    return int(math.floor(B / K) - math.ceil(A / K)) + 1
+    # write your code in Python 3.6
+    return math.floor(B / K) - math.ceil(A / K) + 1
 
 
 result = solution(6, 11, 2)
 print("Sol " + str(result))
+
+"""
+Sol 3
+"""
