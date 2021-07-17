e6f3c1e1a22a70de6b717ab024adf300c3ef3417
Dinakar Maurya <dinkar1708@gmail.com>
2021-07-18 00:54:28 +0900

    Update array_odd_occurrences_in_array.py

--- code/codility/array_odd_occurrences_in_array.py
+++ code/codility/array_odd_occurrences_in_array.py
@@ -1,11 +1,13 @@
 def solution(ar):
     """
+    Time - o(len(ar))
+    Space - o(1)
     Return exactly once
-    :param ar:
+
+    # xor cancels the same elements ie. 2 xor 2 = 0, et the end only unique element will be left.
     :return:
     """
     xor_sum = 0
-    # xor cancels the same elements ie. 2 xor 2 = 0
     for item in ar:
         xor_sum = xor_sum ^ item
     return xor_sum
