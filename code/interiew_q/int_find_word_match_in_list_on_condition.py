84ebea0a06456fbdf79f162b34a335a994e25cc0
Dinakar Maurya <dinkar1708@gmail.com>
2021-07-18 22:50:48 +0900

    Update int_find_word_match_in_list_on_condition.py

--- code/interiew_q/int_find_word_match_in_list_on_condition.py
+++ code/interiew_q/int_find_word_match_in_list_on_condition.py
@@ -19,18 +19,26 @@ for word in list:
     for i in range(len(count_word)):
         if((count_first_input[i] ==0 and count_word[i]!= 0) or (count_word[i] ==0 and count_first_input[i]!= 0)):
             is_same = False
+            print("break for....word, not matching... ", word)
+            break
     if is_same:
         total_match += 1
+print("Total match.....")
 print(total_match)
 
 """
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
 [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
 [4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+break for....word, not matching...  aaaa
 [0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+break for....word, not matching...  bbb
 [0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+break for....word, not matching...  ccc
 [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
 [0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+break for....word, not matching...  ddd
+Total match.....
 2
 """
 
