951352f1fc56c37811e8b7f683798bb7c99a7d8b
Dinakar Maurya <dinkar1708@gmail.com>
2021-07-21 00:39:23 +0900

    Update int_find_word_match_in_list_on_condition.py

--- code/interiew_q/int_find_word_match_in_list_on_condition.py
+++ code/interiew_q/int_find_word_match_in_list_on_condition.py
@@ -4,9 +4,10 @@ def count(str):
         counter[ord(ch) - ord('a')] += 1
     return counter
 
+
 # find the count of words from list which are made of same character
-str = "az"
-list = ["aza", "aaaa", "bbb", "ccc", "azaaaa", "ddd"]
+str = "aza"
+list = ["aza", "aaaa", "bbb", "ccc", "azaaaa", "ddd", "a"]
 # count should be 2 for "aza" and "azaaaa"
 
 count_first_input = count(str)
@@ -14,10 +15,10 @@ print(count_first_input)
 total_match = 0
 for word in list:
     count_word = count(word)
-    print(count_word)
+    print("Word count for ", word, count_word)
     is_same = True
     for i in range(len(count_word)):
-        if((count_first_input[i] ==0 and count_word[i]!= 0) or (count_word[i] ==0 and count_first_input[i]!= 0)):
+        if((count_first_input[i] == 0 and count_word[i] != 0) or (count_word[i] == 0 and count_first_input[i] != 0)):
             is_same = False
             print("break for....word, not matching... ", word)
             break
@@ -27,20 +28,19 @@ print("Total match.....")
 print(total_match)
 
 """
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
 [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
-[4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+Word count for  aza [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+Word count for  aaaa [4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 break for....word, not matching...  aaaa
-[0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+Word count for  bbb [0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 break for....word, not matching...  bbb
-[0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+Word count for  ccc [0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 break for....word, not matching...  ccc
-[5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
-[0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+Word count for  azaaaa [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+Word count for  ddd [0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 break for....word, not matching...  ddd
+Word count for  a [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+break for....word, not matching...  a
 Total match.....
 2
 """
-
-
-        
